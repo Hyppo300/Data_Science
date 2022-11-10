c9166bbf4007d1182ed4a531b8884b34f5d0866b
Hajra Hanif <68047400+Hyppo300@users.noreply.github.com>
2022-11-10 11:53:59 +0500

    Update Queries.sql

--- Week-08/Queries.sql
+++ Week-08/Queries.sql
@@ -16,3 +16,25 @@ CREATE TABLE customers(
   super_id INT
 	
 );
+
+SELECT * FROM public.customers
+ORDER BY cust_id ASC ;
+
+CREATE TABLE menu (
+   itme_id INT AUTO_INCREMENT ,
+	item_name VARCHAR DEFAULT 'Undecided' UNIQUE,
+	item_price INT
+
+);
+
+INSERT INTO public.menu VALUES(1,'Spaghetti',980);
+INSERT INTO public.menu VALUES(4,'Mushroom Pizza',1500);
+INSERT INTO public.menu VALUES(2,'Chowmin',1000);
+INSERT INTO public.menu VALUES(3,'Brocolli Pasta',12000);
+
+select * from public.staff;
+UPDATE public.staff SET supp_id=29 WHERE supplier='Hang';
+
+
+select * from public.menu;
+DELETE FROM public.menu WHERE itme_id=2 AND item_name='Chowmin';
